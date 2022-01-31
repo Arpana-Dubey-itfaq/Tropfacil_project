2c1d2c421c0147b1342019ed53f56d9fb83ea021
arpana <[arpanavibha92@gmail.com]>
2022-01-31 18:12:23 +0530

    Design changes

---
+++ app/src/main/java/com/tropfacil/Sign_up_Screen.kt
@@ -0,0 +1,11 @@
+package com.tropfacil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_up_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_screen)
+    }
+}
